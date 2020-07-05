0350cf578e59f0e1c75a8ead568a5e046c9113be
IamFred14 <fredericopinto7@gmail.com>
2020-07-05 13:29:04 +0100

    simple calculator with 2 numbers finished

--- simple_calculator.py
+++ simple_calculator.py
@@ -1,19 +1,49 @@
-# Quick note: Mano, do you understand how naming conventions work? 
-# For exemple: variable naming convention for javascript is camel case 
-# such as " var myCar". But in python is "my_car", which is called sanake case.
-# Files also have naming convention. For instance, when you have a file with a class Car
-# you name the file of Car.py, but you have a file with a function, say "def get_car():"
-# you name it car.py  
+# Function definition for the math operations.
 
-# Mano, my clients has a proble: he goes shopping his groceries every other day and he
-# hates the fact that he has to work out the expenses and how much money he has to give
-# to the counter in his head. So, he's looking for a simple calculator that helps him
-# with the boring repetitive process. Could you build a simple calculator, easy to use? 
+def add(num1,num2):
+    print(num1+num2)
+def sub(num1,num2):
+    print(num1-num2)
+def mult(num1,num2):
+    print(num1*num2)
+def div(num1,num2):
+  try:
+      print(num1/num2)
+  except ZeroDivisionError as err:
+      print(err)
 
-#### The calculater shoud have addiction, multplication, subtraction and division functions
-### so that when the clients needs the total of his workouts, he only needs to do the following
+# Defining a boolean exit button.
+leave = False
+# while loop in function to our boolean button
+while leave == False :
+  
+    # Getting input from the user(The input starts all as str but is converted to float after).
+    try:
+      numA, oper, numB = input("Ready: ").split()
+    except ValueError as err:
+      print(err)
+      break
+    
+    num1, num2 = float(numA), float(numB)
+  
 
-# for example: total = add function + subtruction function
-### Also, have in mind that the client should have a clear message on what operation he's doing. 
+    #conditionals for the final result.
 
-## hint: use of functions, loops and conditionals
+    if oper == "+":
+      add(num1, num2)
+    elif oper == "-":
+      sub(num1,num2)
+    elif oper == "x" or oper == "*":
+      mult(num1,num2)
+    elif oper== "/":
+      div(num1,num2)
+    else:
+      print("Invalid operator, please try again!")
+
+    get_out = input("Type exit if you wish to leave, or press enter to continue")
+    if get_out == "exit" or get_out == "Exit":
+      leave = True
+    else :
+      leave = False
+
+   
